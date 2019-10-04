d9bdadf635fe9dfd3bf106986daafb5a9f4d2b0e
engrjepmanzanillo <engr.jeffmanzanillo@gmail.com>
2019-10-04 13:53:05 +0800

    initial commit

---
+++ day1_datatypes.py
@@ -0,0 +1,24 @@
+# https://www.hackerrank.com/challenges/30-data-types/problem
+"""
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i_2, d_2, s_2 = int(12), float(4.0), str('is the best place to learn and practice coding!')
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + i_2)
+# Print the sum of the double variables on a new line.
+print(d + d_2)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s_2)
+
---
+++ day2_operators.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+"""Objective
+In this challenge, you'll work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!"""
+
+"""
+Sample Input
+
+12.00
+20
+8
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost*tip_percent/100
+    tax = meal_cost*tax_percent/100
+    print(round(meal_cost + tip + tax))
+
+if __name__ == '__main__':
+    meal_cost = float(12.00)
+
+    tip_percent = int(20)
+
+    tax_percent = int(8)
+
+    solve(meal_cost, tip_percent, tax_percent)
